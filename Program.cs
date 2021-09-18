5d135faa87272a9bfed881a31c918cdb5ce0eabe
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-09-17 21:54:31 -0300

    Resolução Exercício grausCF

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperaturaC, temperaturaF;
+
+            Console.Write("Qual a temperatura °C:");
+            temperaturaC = Convert.ToDouble(Console.ReadLine());
+
+            temperaturaF = temperaturaC * 1.8 + 32;
+
+            Console.WriteLine($"{temperaturaC}°C = {temperaturaF}F°");
+        }
+    }
+}
